9272b0d8b2d36977b5912d15c4e93480fd718b7a
Mark Veltzer <mark.veltzer@gmail.com>
2023-04-26 08:55:29 +0300

    no commit message given

---
+++ exercises/06_delete_pod/app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import time
+import sys
+import signal
+
+"""
+This is an example of how to do corrent printing to stdout in docker
+since stdout is buffered by default.
+
+Two ways are shown here:
+- sys.stdout.reconfigure
+- sys.stdout.flush
+any of them would work on its own.
+"""
+
+signal.signal(signal.SIGINT, self.exit_gracefully)
+i=0
+while True:
+  print(f"i is {i}...")
+  sys.stdout.flush()
+  time.sleep(1)
+  i+=1
