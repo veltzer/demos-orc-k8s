9f583f866a957d46649c525b377e2bdbc48033fd
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-25 17:47:02 +0200

    no commit message given

--- exercises/01_run_first_public_image/get_page.sh
+++ exercises/01_run_first_public_image/get_page.sh
@@ -2,4 +2,4 @@
 
 # find the ip of the host of the deployment
 ip=$(kubectl get pod -l app=nginx -o jsonpath="{.items[0].status.podIP}")
-wget --quiet -O - $ip:80 | head
+wget --quiet -O - "${ip}:80" | head
