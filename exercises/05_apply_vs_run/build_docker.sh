194be1c2294e1a8d764de245c73c825bfd72d9dc
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-27 20:11:18 +0200

    no commit message given

--- exercises/05_apply_vs_run/build_docker.sh
+++ exercises/05_apply_vs_run/build_docker.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash -e
 image_name=$(basename "${PWD}")
-docker build --tag "${image_name}:latest" .
+docker buildx build --tag "${image_name}:latest" .
--- exercises/05_apply_vs_run/run_docker.sh
+++ exercises/05_apply_vs_run/run_docker.sh
@@ -2,5 +2,3 @@
 image_name=$(basename "${PWD}")
 # docker run --network=host -it "${image_name}"
 docker run -p 8080:8080 -it "${image_name}"
-# this DOES NOT work
-# docker run -p 8080 -it "${image_name}"
