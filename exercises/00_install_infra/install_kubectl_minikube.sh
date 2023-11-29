2cbbdb731c8cb71974645e82b803c7d120cc7fe7
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-29 05:16:50 +0200

    no commit message given

--- exercises/00_install_infra/install_kubectl_minikube.sh
+++ exercises/00_install_infra/install_kubectl_minikube.sh
@@ -3,11 +3,13 @@
 folder="${HOME}/binaries"
 if [ -d "${folder}" ]
 then
+	echo "removing previous [${folder}]..."
 	rm -rf "${folder}"
 fi
+echo "making folder [${folder}]..."
 mkdir "${folder}"
 
-echo "installing kubectl"
+echo "installing kubectl..."
 # instructions for installing k8s are at:
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 version=$(curl --silent --location "https://dl.k8s.io/release/stable.txt")
@@ -16,12 +18,13 @@ executable="${folder}/kubectl"
 curl --location --silent --output "${executable}" "https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl"
 chmod +x "${executable}"
 
-echo "installing minikube"
+echo "installing minikube..."
 # instructions for installing minikube are are:
 # https://minikube.sigs.k8s.io/docs/start/
+version="latest"
 executable="${folder}/minikube"
-curl --location --silent --output "${executable}" "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+curl --location --silent --output "${executable}" "https://storage.googleapis.com/minikube/releases/${version}/minikube-linux-amd64"
 chmod +x "${executable}"
 
 echo "adding ~/binaries to your PATH"
-echo "export PATH=\"\$\{HOME\}/binaries:\$\{PATH\}\"" >> ~/.bashrc
+echo "export PATH=\"\${HOME}/binaries:\${PATH}\"" >> ~/.bashrc
