2a910593680a4ff2589cc75007512ae7f6e871bc
Mark Veltzer <mark.veltzer@gmail.com>
2023-11-29 05:12:46 +0200

    no commit message given

--- exercises/00_install_infra/install_kubectl_minikube.sh
+++ exercises/00_install_infra/install_kubectl_minikube.sh
@@ -8,10 +8,20 @@ fi
 mkdir "${folder}"
 
 echo "installing kubectl"
-# instructions for installing k8s are at
+# instructions for installing k8s are at:
 # https://kubernetes.io/docs/tasks/tools/install-kubectl-linux/
 version=$(curl --silent --location "https://dl.k8s.io/release/stable.txt")
 echo "installing latest version ${version}"
 executable="${folder}/kubectl"
 curl --location --silent --output "${executable}" "https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl"
 chmod +x "${executable}"
+
+echo "installing minikube"
+# instructions for installing minikube are are:
+# https://minikube.sigs.k8s.io/docs/start/
+executable="${folder}/minikube"
+curl --location --silent --output "${executable}" "https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64"
+chmod +x "${executable}"
+
+echo "adding ~/binaries to your PATH"
+echo "export PATH=\"\$\{HOME\}/binaries:\$\{PATH\}\"" >> ~/.bashrc
