9d3b2a343aab1f853d35ac96be1dd4e8432251d5
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-13 10:29:51 +0200

    no commit message given

--- exercises/02_run_first_public_image/get_page.sh
+++ exercises/02_run_first_public_image/get_page.sh
@@ -3,4 +3,4 @@
 # find the ip of the host of the deployment
 ip=$(kubectl get pod -l app=nginx -o jsonpath="{.items[0].status.podIP}")
 echo "ip is ${ip}"
-wget --quiet -O - "${ip}:80" | head
+echo "curl --silent http://${ip} | head; exit" | minikube ssh
