686ca37bffeaac2de4335c649634bf578b9bde94
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-10 15:33:56 +0300

    no commit message given

--- exercises/12_limit_memory_of_pod/app.py
+++ exercises/12_limit_memory_of_pod/app.py
@@ -5,6 +5,7 @@ Application which grows in memory requirements all the time
 """
 
 import time
+import sys
 import random
 
 big_array = []
@@ -12,4 +13,5 @@ while True:
     add_array = [random.randint(0, 100) for _ in range(300000)]
     big_array.extend(add_array)
     print(f"size of array is [{len(big_array)}]...")
+    sys.stdout.flush()
     time.sleep(1)
