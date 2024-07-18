14d1d511cf715c42b5c5443abd86e82d6a98d897
Mark Veltzer <mark.veltzer@gmail.com>
2024-07-18 08:54:57 +0300

    no commit message given

--- exercises/20_using_helm/connect.sh
+++ exercises/20_using_helm/connect.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-export MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace default my-release-mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d)
+MYSQL_ROOT_PASSWORD=$(kubectl get secret --namespace default my-release-mysql -o jsonpath="{.data.mysql-root-password}" | base64 -d)
 # echo "password is ${MYSQL_ROOT_PASSWORD}"
 
 # delete the old my-release-mysql-client pod
@@ -10,4 +10,4 @@ kubectl delete pods my-release-mysql-client || true
 # kubectl run my-release-mysql-client --rm --tty -i --restart="Never" --image "docker.io/bitnami/mysql" --env "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" --command -- bash
 
 # connect to mysql
-kubectl run my-release-mysql-client --rm --tty -i --restart="Never" --image "docker.io/bitnami/mysql" --namespace default --env "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" --command -- bash -c 'mysql -h my-release-mysql -uroot --password=${MYSQL_ROOT_PASSWORD}'
+kubectl run my-release-mysql-client --rm --tty -i --restart="Never" --image "docker.io/bitnami/mysql" --namespace default --env "MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}" --command -- bash -c 'mysql -h my-release-mysql -uroot --password=\$\{MYSQL_ROOT_PASSWORD\}'
