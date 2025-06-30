384f7cbcdb21e6e6541239bc0005a7fe0c875e48
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-30 13:47:47 +0300

    fixed up deps

--- config/python.py
+++ config/python.py
@@ -1,19 +1,16 @@
 """ python deps for this project """
 
-build_requires: list[str] = [
+import config.shared
+
+install_requires: list[str] = [
     "flask",
     "mysql-connector-python",
     "pyvardump",
     "requests",
-    "types-requests",
-
-    "pydmt",
-    "pymakehelper",
-    "pycmdtools",
-
-    "pylint",
-    "pytest",
-    "mypy",
-    "ruff",
 ]
-requires = build_requires
+build_requires: list[str] = config.shared.BUILD
+test_requires: list[str] = config.shared.TEST
+types_requires: list[str] = [
+    "types-requests",
+]
+requires = install_requires + build_requires + test_requires + types_requires
