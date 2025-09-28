ba0cda27e67bb31f56a3122ff716fed9eed66d73
Mark Veltzer <mark.veltzer@gmail.com>
2025-09-28 16:24:38 +0300

    no commit message given

--- exercises/02_run_first_public_image/get_page.sh
+++ exercises/02_run_first_public_image/get_page.sh
@@ -4,6 +4,6 @@
 for i in {0..2}
 do
 	ip=$(kubectl get pod -l app=nginx -o jsonpath="{.items[${i}].status.podIP}")
-	echo "ip is ${ip}"
+	echo "ip is [${ip}]"
 	echo "curl --silent http://${ip} | head; exit" | minikube ssh
 done
